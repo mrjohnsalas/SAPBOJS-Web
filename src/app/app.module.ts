6c1a807e747c0852dcbeced06d870785a36f8382
John Salas <salas.john@hotmail.com>
2019-10-10 14:14:18 -0500

    2019-10-10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { MenuComponent } from './shared/menu/menu.component';
-import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    MenuComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
